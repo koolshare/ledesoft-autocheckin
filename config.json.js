5cad602cd2ccaf1c2bdaa3c894ca190ebd59da86
Carseason <757841203@qq.com>
2022-02-09 04:22:25 +0800

    update 2.2

--- config.json.js
+++ config.json.js
@@ -1,9 +1,9 @@
 {
-    "version": "2.1",
-        "md5": "390bea0e2d0970d31a4221ada51dbc1c",
-            "home_url": "Module_autocheckin.asp",
-                "title": "签到狗3.0",
-                    "description": "每日批量自动签到",
-                        "changelog": "修正更新",
-                            "build_date": "2021-02-08_23:55:15"
+"version":"2.2",
+"md5":"862758bae4326448162140b21241b30a",
+"home_url":"Module_autocheckin.asp",
+"title":"签到狗3.0",
+"description":"每日批量自动签到",
+"changelog":"修正更新",
+"build_date":"2022-02-09_04:21:35"
 }
